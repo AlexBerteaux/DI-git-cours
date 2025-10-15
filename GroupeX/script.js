f022a5bcb6720865eba920f925df844e02633d8f
Squidbaba11 <basilejossaume@gmail.com>
2025-10-15 07:56:04 +0000

    js_V4

--- GroupeX/script.js
+++ GroupeX/script.js
@@ -1,34 +1,11 @@
-<!DOCTYPE html>
-</head>
-<body>
-    <h1>GroupeX</h1>
-    
-    <div class="paragraph" data-name="Basile">
-        <strong>Basile</strong> - DI25.
-    </div>
-    
-    <div class="paragraph" data-name="Omar">
-        <strong>Omar</strong> - DI25.
-    </div>
-    
-    <div class="paragraph" data-name="Sevan">
-        <strong>Sevan</strong> - DI25.
-    </div>
-    
-    <div class="paragraph" data-name="Baptiste">
-        <strong>Baptiste</strong> - DI25.
-    </div>
-    
-    <div id="display"></div>
-    
+  
     <script>
         const paragraphs = document.querySelectorAll('.paragraph');
         const display = document.getElementById('display');
         
         paragraphs.forEach(p => {
             p.addEventListener('mouseenter', function() {
-                const name = this.getAttribute('data-name');
-                display.textContent = name;
+                display.textContent = id;
             });
             
             p.addEventListener('mouseleave', function() {
