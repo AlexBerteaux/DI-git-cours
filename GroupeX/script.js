14fc8186cfb43d55d38d3879ac8f61ca2af12aeb
Squidbaba11 <basilejossaume@gmail.com>
2025-10-15 07:51:29 +0000

    js V3

--- GroupeX/script.js
+++ GroupeX/script.js
@@ -3,20 +3,20 @@
 <body>
     <h1>GroupeX</h1>
     
-    <div class="paragraph" data-name="Sevan">
-        <strong>Sevan</strong> - DI25.
-    </div>
-    
-    <div class="paragraph" data-name="Baptiste">
-        <strong>Baptiste</strong> - DI25.
+    <div class="paragraph" data-name="Basile">
+        <strong>Basile</strong> - DI25.
     </div>
     
     <div class="paragraph" data-name="Omar">
         <strong>Omar</strong> - DI25.
     </div>
     
-    <div class="paragraph" data-name="Basile">
-        <strong>Basile</strong> - DI25.
+    <div class="paragraph" data-name="Sevan">
+        <strong>Sevan</strong> - DI25.
+    </div>
+    
+    <div class="paragraph" data-name="Baptiste">
+        <strong>Baptiste</strong> - DI25.
     </div>
     
     <div id="display"></div>
